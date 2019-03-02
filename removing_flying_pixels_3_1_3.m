b62ee92489a54050bf86f2008122d6589dc48b50
marangiop <marangiopaolo@gmail.com>
2019-03-02 17:31:26 +0000

    Added code to remove 3D points in xyz in iterative loop

--- removing_flying_pixels_3_1_3.m
+++ removing_flying_pixels_3_1_3.m
@@ -5,19 +5,12 @@ points = office{27}.Location; % Extracting the xyz data
 
 pc = pointCloud(points, 'Color', rgb); % Creating a point-cloud variable
 
-%what do we mean by 3D point here?
-%how do I recreate the top right image on page 2?
-
-% points_reshaped=reshape(points,[3,307200]);this is probably wrong
 points_reshaped= points.'; %transpose of points array
-
 
 idx= isnan(points_reshaped); %replacing Nan by 0...is this correct?
 points_reshaped(idx)= 0;
 
 rgb_reshaped= rgb.';
-%rgb_reshaped = reshape(rgb,[3,307200]); this is probably wrong
-
 
 K_neighbours=27; %Number of neighbours for which we calculate the distance from a given point
 number_points_checked=307200; %Total number of points we check in the loop below
@@ -43,67 +36,30 @@ number_neighbours_threshold = 1;
 for i = 1:number_points_checked
    point = [points_reshaped(1,i) points_reshaped(2,i) points_reshaped(3,i)];
    [indices(:,i),dists(:,i)] = findNearestNeighbors(pc,point,K_neighbours);
-  %filtered_distance(:,i) = dists(:,i)<distance_threshold;
    filtered_distance = dists(:,i)<distance_threshold;
    number_neighbours_within_distance=sum(filtered_distance(:) == 1);
    if number_neighbours_within_distance<number_neighbours_threshold
        rgb_reshaped(1,i)=0;
        rgb_reshaped(2,i)=0;
        rgb_reshaped(3,i)=0; 
+       
+       points_reshaped(1,i)=0;
+       points_reshaped(2,i)=0;
+       points_reshaped(3,i)=0;
+       
    end
         
 end
  
-%rgb_reshaped_back = reshape(rgb,[307200, 3]); this is wrong - no black
-%colour on image
-
 rgb_reshaped_back=rgb_reshaped.'; %transpose
+point_reshaped_back=points_reshaped.'; %transpose
 
-updated_pc = pointCloud(points, 'Color', rgb_reshaped_back); % Creating a point-cloud variable
+updated_pc = pointCloud(point_reshaped_back, 'Color', rgb_reshaped_back); % Creating a point-cloud variable
 figure(1)
 pcshow(updated_pc)
 figure(2)
 imag2d(rgb_reshaped_back) 
 
-%
 
-
-
-
-
-%{
-
-pc = pointCloud(point, 'Color', rgb); % Creating a point-cloud variable
-figure(1)
-pcshow(point)
-figure(2)
-imag2d(rgb) % Shows the 2D image
-
-%}
-
-
-
-%z = point(:,3); %get the z dimension
-%y = point(:,2);%get the y dimension
-
-
-
-%k = dsearchn(z,T,y)
-%k = dsearchn(X,T,XI)
-
-%X is an m-by-n matrix representing m points in n-dimensional space
-%XI is a p-by-n matrix, representing p points in n-dimensional space
-%T N-D Delaunay triangulation???
-
-
-%{
-for i = 1:length(z) %for each 3D point (in z dimension)
-
-%}
-    
-
-
-    
-    
 
 
